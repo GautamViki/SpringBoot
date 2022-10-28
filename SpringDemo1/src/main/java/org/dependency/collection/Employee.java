8aa71951c5e5a9ef9fa97d6232a67741241f1e75
GautamViki <vikacse@gmail.com>
2022-10-28 10:04:32 +0530

    collection injectoin

---
+++ SpringDemo1/src/main/java/org/dependency/collection/Employee.java
@@ -0,0 +1,54 @@
+package org.dependency.collection;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Employee {
+    private String name;
+    private List<String> phone;
+    private Set<String> address;
+    private Map<String, String> course;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPhone() {
+        return phone;
+    }
+
+    public void setPhone(List<String> phone) {
+        this.phone = phone;
+    }
+
+    public Set<String> getAddress() {
+        return address;
+    }
+
+    public void setAddress(Set<String> address) {
+        this.address = address;
+    }
+
+    public Map<String, String> getCourse() {
+        return course;
+    }
+
+    public void setCourse(Map<String, String> course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", phone=" + phone +
+                ", address=" + address +
+                ", course=" + course +
+                '}';
+    }
+}
--- SpringDemo1/src/main/java/org/depndencyCollection/Employee.java
+++
@@ -1,4 +0,0 @@
-package org.depndencyCollection;
-
-public class Employee {
-}
